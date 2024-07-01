94efcba65e3b7aea52f1b4f1c4d90bd43dcc0cee
kaestle <j.kaestle@habisreutinger.de>
2024-07-01 08:31:17 +0200

    Update paragraph alignment in About component
    The 'text-center' class was removed from the paragraph in the About component, which affected the alignment of the text. Now, the alignment of the paragraph text has been updated to a 'text-justify' alignment for better readability and aesthetic appeal on the webpage.

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,7 +3,7 @@ export default function About () {
     return (
         <div id="about"  className="container mx-auto py-16 px-4 ">
             <h2 className="text-4xl font-bold text-center mb-8">Über Uns</h2>
-            <p className="text-lg text-center text-justify mx-auto" >
+            <p className="text-lg text-justify  mx-auto" >
                 Wir sind eine familiär geführte Hausarztpraxis in der 3. Generation in Wangen im Allgäu. Sie finden uns im Zentrum knapp außerhalb des historischen Stadtkerns schräg gegenüber des Ravensburger Tores.
                 Neben den üblichen allgemeinmedizinischen Leistungen legen wir besonderen Wert auf Gesundheitsvorsorge und eine individuelle ganzheitliche Beratung. Profitieren Sie dabei von der Verzahnung schulmedizinischer und komplementär- bzw. alternativmedizinischer Diagnose- und Behandlungsverfahren. Zu diesen zählen Akupunktur, Biologische Medizin, spezielle Entgiftungsverfahren, Sanierung der Darmflora und andere.
                 In unserer eigens dafür eingerichteten komplementärmedizinischen Sprechstunde am Montag Abend nehmen wir uns viel Zeit für Erkrankungen, bei denen die Schulmedizin nicht ausreichend helfen kann. Dies sind z.B. Beschwerden ohne organischen Befund, chronische Müdigkeit, Infektanfälligkeit, chronische Vereiterungen der Nasennebenhöhlen, unklare Allergien, Verdauungsbeschwerden (Blähungen, Verstopfung, Durchfall).
